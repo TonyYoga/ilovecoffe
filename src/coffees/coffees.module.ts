c113bb3608978db45b96dc6bfa933e828fb70bf4
Anton Konkin <konkin.anton@gmail.com>
2022-05-08 16:29:35 +0300

    Class provider patern

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { env } from 'process';
 import { Event } from 'src/events/entities/event.entity';
 import { COFFEE_BRANDS } from './coffees.constants';
 import { CoffeesController } from './coffees.controller';
@@ -7,11 +8,21 @@ import { CoffeesService } from './coffees.service';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 
+class ConfigService {}
+class DevConfigService {}
+class ProdConfigService {}
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   controllers: [CoffeesController],
   providers: [
     CoffeesService,
+    {
+      provide: ConfigService,
+      useClass:
+        process.env.NODE_ENV === 'development'
+          ? DevConfigService
+          : ProdConfigService,
+    },
     // {
     // provide: CoffeesService,
     // useClass: CoffeesService,
